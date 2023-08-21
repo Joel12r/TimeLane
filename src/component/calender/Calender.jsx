ea79a2fcc0574f95820f5f47c04a34bb2c4a15fb
Joel12r <joelwalker152@gmail.com>
2023-08-21 11:12:12 +0200

    feat : added features to the card list

--- src/component/calender/Calender.jsx
+++ src/component/calender/Calender.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaEllipsisV, FaRegTrashAlt } from "react-icons/fa";
-import "../../App.css"
+import { FaEllipsisV, FaRegTrashAlt, FaFeatherAlt, FaPencilAlt, FaSave, FaAlignLeft } from "react-icons/fa";
+import '../../../src/App.css'
 
 export default function Calendar() {
   const [currentDate, setCurrentDate] = useState(new Date());
@@ -13,6 +13,15 @@ export default function Calendar() {
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [deletingItemIndex, setDeletingItemIndex] = useState(null);
   const [showDeleteItemModal, setShowDeleteItemModal] = useState(false);
+  const [selectedListForAlignment, setSelectedListForAlignment] = useState(null);
+  const [showItemModal, setShowItemModal] = useState(false);
+  const [selectedCardIndex, setSelectedCardIndex] = useState(null);
+  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
+  const [itemDescription, setItemDescription] = useState('');
+  const [showEditorToolbar, setShowEditorToolbar] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentDate(new Date());
@@ -53,20 +62,21 @@ export default function Calendar() {
     setDeletingCardIndex(index);
     setShowDeleteModal(true);
   };
-
-  const handleDeleteItem = (index) => {
-    setDeletingItemIndex(index);
+  // Item delete
+  const handleDeleteItem = (cardIndex, itemIndex) => {
+    setDeletingCardIndex(cardIndex);
+    setDeletingItemIndex(itemIndex);
     setShowDeleteItemModal(true);
   };
-  const confirmDeleteItem = (cardIndex, itemIndex) => {
+
+  const confirmDeleteItem = () => {
     const updatedCards = [...cards];
-    updatedCards[cardIndex].items.splice(itemIndex, 1);
+    updatedCards[deletingCardIndex].items.splice(deletingItemIndex, 1);
     setCards(updatedCards);
     setShowDeleteItemModal(false);
     setDeletingCardIndex(null);
     setDeletingItemIndex(null);
   };
-
 
   const confirmDelete = () => {
     const updatedCards = [...cards];
@@ -81,6 +91,20 @@ export default function Calendar() {
     updatedCards[index].items.push(newItem);
     updatedCards[index].newItem = '';
     setCards(updatedCards);
+  };
+  // list popup model 
+  const handleItemClicked = (cardIndex, itemIndex) => {
+    setSelectedCardIndex(cardIndex);
+    setSelectedItemIndex(itemIndex);
+    setItemDescription(cards[cardIndex].items[itemIndex].description || ''); // Set initial description
+    setShowItemModal(true);
+
+  };
+  const handleSaveDescription = () => {
+    const updatedCards = [...cards];
+    updatedCards[selectedCardIndex].items[selectedItemIndex].description = itemDescription;
+    setCards(updatedCards);
+    setShowItemModal(false);
   };
 
   return (
@@ -97,7 +121,7 @@ export default function Calendar() {
           <div className="col-lg-12" style={{ overflowX: "auto", whiteSpace: "nowrap" }}>
             {cards.map((card, index) => (
               <div className="col-lg-4 d-inline-block" key={index}>
-                <div className="card mx-2">
+                <div className="card mx-2" style={{ backgroundColor: "transparent", border: "none" }}>
                   <div className="card-body">
                     <div className="row">
                       <div className="col-lg-9 col-sm-8 mt-2 mb-3">
@@ -112,11 +136,23 @@ export default function Calendar() {
                       <div className="container">
                         <div className="row">
                           <div className="col-sm-6">
+
                             {card.items.map((item, itemIndex) => (
-                              <li key={itemIndex} style={{ listStyle: "none", }}>{item}
-                              <button className="btn col-sm-4" style={{ marginLeft: "10rem" }} onClick={() => handleDeleteItem(index)}><FaRegTrashAlt /></button>
+                              <li
+                                key={itemIndex}
+                                className="mb-5 clickable-item"
+                                style={{ listStyle: "none", border: "dotted", width: "20rem", height: "4rem" }}
+                                onClick={() => handleItemClicked(index, itemIndex)} // Add this line
+                              >
+                                {item}
+
+                                <div className="mt-2 mb-3">
+                                  <button type="button" className="btn btn-sm"> <FaAlignLeft /></button>
+
+                                </div>
                               </li>
                             ))}
+
                           </div>
                         </div>
                       </div>
@@ -166,7 +202,7 @@ export default function Calendar() {
                 </div>
                 <div className="modal-footer">
                   <button type="button" className="btn btn-secondary" onClick={() => setShowModal(false)}>Cancel</button>
-                  <button type="button" className="btn btn-primary" onClick={handleSaveCard}>Save</button>
+                  <button type="button" className="btn btn-primary" onClick={handleSaveCard}><FaSave /></button>
                 </div>
               </div>
             </div>
@@ -181,7 +217,7 @@ export default function Calendar() {
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title">Confirm Delete</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => setShowDeleteModal(false)}>
+                <button type="button" className="btn btn-sm btn-danger close" data-dismiss="modal" aria-label="Close" onClick={() => setShowDeleteModal(false)}>
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
@@ -203,7 +239,7 @@ export default function Calendar() {
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title">Confirm Delete</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => setShowDeleteItemModal(false)}>
+                <button type="button" className="btn btn-sm btn-danger close" data-dismiss="modal" aria-label="Close" onClick={() => setShowDeleteItemModal(false)}>
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
@@ -211,8 +247,107 @@ export default function Calendar() {
                 Are you sure you want to delete this item from the list?
               </div>
               <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" onClick={() => setShowDeleteItemModal(false)}>Cancel</button>
-                <button type="button" className="btn btn-danger" onClick={() => confirmDeleteItem(deletingItemIndex)}>Delete</button>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={() => {
+                    setShowDeleteItemModal(false);
+                    setDeletingCardIndex(null);
+                    setDeletingItemIndex(null);
+                  }}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={confirmDeleteItem}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showItemModal && (
+        <div className="modal col-lg-12" tabIndex="-1" role="dialog" style={{ display: "block", backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+          <div className="modal-dialog modal-xl" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Item Details</h5>
+                <button type="button" className=" btn btn-sm btn-danger close" data-dismiss="modal" aria-label="Close" onClick={() => setShowItemModal(false)}>
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-lg-6 mb-5">
+                      {/* Display item details based on selectedCardIndex and selectedItemIndex */}
+                      {selectedCardIndex !== null && selectedItemIndex !== null && (
+                        <>
+                          <p>Name: {cards[selectedCardIndex].items[selectedItemIndex]}</p>
+
+                          <p>Description: {itemDescription}</p>
+                          {/* Item description input */}
+                          <textarea
+                            className="form-control"
+                            placeholder="Enter item description"
+                            value={itemDescription}
+                            onChange={(e) => setItemDescription(e.target.value)}
+                          />
+                          {/* <button className="btn btn-primary mt-2" onClick={handleSaveDescription}>Save</button> */}
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                {showDeleteItemModal && (
+                  <div className="modal" tabIndex="-1" role="dialog" style={{ display: "block", backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                      <div className="modal-content">
+                        <div className="modal-header">
+                          <h5 className="modal-title">Confirm Delete</h5>
+                          <button type="button" className="btn btn-sm btn-danger close" data-dismiss="modal" aria-label="Close" onClick={() => setShowDeleteItemModal(false)}>
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div className="modal-body ">
+                          Are you sure you want to delete this item from the list?
+                        </div>
+                        <div className="modal-footer">
+                          <button
+                            type="button"
+                            className="btn btn-secondary"
+                            onClick={() => {
+                              setShowDeleteItemModal(false);
+                              setDeletingCardIndex(null);
+                              setDeletingItemIndex(null);
+                            }}
+                          >
+                            Cancel
+                          </button>
+                          <button
+                            type="button"
+                            className="btn btn-danger"
+                            onClick={confirmDeleteItem}
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+                <button
+                  className="btn btn-danger mt-3"
+                  style={{ marginLeft: "66rem" }}
+                  onClick={() => handleDeleteItem(selectedCardIndex, selectedItemIndex)}
+                >
+                  <FaRegTrashAlt />
+                </button>
               </div>
             </div>
           </div>
